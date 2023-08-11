a39706e4ec534d1212888b1464488227ca8a24a8
Andy Liu <andy@madmachine.io>
2023-08-11 11:56:47 +0800

    Init CMemTester
    Signed-off-by: Andy Liu <andy@madmachine.io>

---
+++ Sources/CMemTester/memtester.c
@@ -0,0 +1,145 @@
+/*
+ * memtester version 4
+ *
+ * Very simple but very effective user-space memory tester.
+ * Originally by Simon Kirby <sim@stormix.com> <sim@neato.org>
+ * Version 2 by Charles Cazabon <charlesc-memtester@pyropus.ca>
+ * Version 3 not publicly released.
+ * Version 4 rewrite:
+ * Copyright (C) 2004-2020 Charles Cazabon <charlesc-memtester@pyropus.ca>
+ * Licensed under the terms of the GNU General Public License version 2 (only).
+ * See the file COPYING for details.
+ *
+ */
+
+#define __version__ "4.5.1"
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#include "types.h"
+#include "sizes.h"
+#include "tests.h"
+#include "output.h"
+
+#define EXIT_FAIL_NONSTARTER    0x01
+#define EXIT_FAIL_ADDRESSLINES  0x02
+#define EXIT_FAIL_OTHERTEST     0x04
+
+struct test tests[] = {
+    { "Random Value", test_random_value },
+    { "Compare XOR", test_xor_comparison },
+    { "Compare SUB", test_sub_comparison },
+    { "Compare MUL", test_mul_comparison },
+    { "Compare DIV",test_div_comparison },
+    { "Compare OR", test_or_comparison },
+    { "Compare AND", test_and_comparison },
+    { "Sequential Increment", test_seqinc_comparison },
+    { "Solid Bits", test_solidbits_comparison },
+    { "Block Sequential", test_blockseq_comparison },
+    { "Checkerboard", test_checkerboard_comparison },
+    { "Bit Spread", test_bitspread_comparison },
+    { "Bit Flip", test_bitflip_comparison },
+    { "Walking Ones", test_walkbits1_comparison },
+    { "Walking Zeroes", test_walkbits0_comparison },
+#ifdef TEST_NARROW_WRITES
+    { "8-bit Writes", test_8bit_wide_random },
+    { "16-bit Writes", test_16bit_wide_random },
+#endif
+    { NULL, NULL }
+};
+
+/* Sanity checks and portability helper macros. */
+#define check_posix_system()
+
+int memtester_pagesize(void) {
+    printf("sysconf(_SC_PAGE_SIZE) not supported; using pagesize of 8192\n");
+    return 8192;
+}
+
+/* Some systems don't define MAP_LOCKED.  Define it to 0 here
+   so it's just a no-op when ORed with other constants. */
+#ifndef MAP_LOCKED
+  #define MAP_LOCKED 0
+#endif
+
+/* Function declarations */
+void usage(char *me);
+
+/* Global vars - so tests have access to this information */
+int use_phys = 0;
+off_t physaddrbase = 0;
+
+/* Function definitions */
+void usage(char *me) {
+    fprintf(stderr, "\n"
+            "Usage: %s [-p physaddrbase [-d device] [-u]] <mem>[B|K|M|G] [loops]\n",
+            me);
+    exit(EXIT_FAIL_NONSTARTER);
+}
+
+int test_ram_start(void *aligned, unsigned long bufsize, unsigned long loops) {
+    ul loop, i;
+    size_t halflen, count;
+
+    ulv *bufa, *bufb;
+
+    int exit_code = 0;
+    ul testmask = 0;
+
+    out_initialize();
+
+    printf("memtester version " __version__ " (%d-bit)\n", UL_LEN);
+    printf("Copyright (C) 2001-2020 Charles Cazabon.\n");
+    printf("Licensed under the GNU General Public License version 2 (only).\n");
+    printf("\n");
+
+    halflen = bufsize / 2;
+    count = halflen / sizeof(ul);
+    bufa = (ulv *) aligned;
+    bufb = (ulv *) ((size_t) aligned + halflen);
+
+    for(loop=1; ((!loops) || loop <= loops); loop++) {
+        printf("Loop %lu", loop);
+        if (loops) {
+            printf("/%lu", loops);
+        }
+        printf(":\n");
+        printf("  %-20s: ", "Stuck Address");
+        fflush(stdout);
+        if (!test_stuck_address(aligned, bufsize / sizeof(ul))) {
+             printf("ok\n");
+        } else {
+            exit_code |= EXIT_FAIL_ADDRESSLINES;
+        }
+        for (i=0;;i++) {
+            if (!tests[i].name) break;
+            /* If using a custom testmask, only run this test if the
+               bit corresponding to this test was set by the user.
+             */
+            if (testmask && (!((1 << i) & testmask))) {
+                continue;
+            }
+            printf("  %-20s: ", tests[i].name);
+            fflush(stdout);
+            if (!tests[i].fp(bufa, bufb, count)) {
+                printf("ok\n");
+            } else {
+                exit_code |= EXIT_FAIL_OTHERTEST;
+            }
+            fflush(stdout);
+        }
+        printf("\n");
+        fflush(stdout);
+    }
+    printf("Done.\n");
+    fflush(stdout);
+    return exit_code;
+}
